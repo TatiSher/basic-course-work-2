5f5fda963f4a82655670454d1a823951ea2787a1
Tatiana <tanja6122212@list.ru>
2022-02-06 23:23:32 +0900

    Initial commit

---
+++ src/main/java/pro/sky/basiccoursework2/BasicCourseWork2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.basiccoursework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCourseWork2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCourseWork2Application.class, args);
+	}
+
+}
